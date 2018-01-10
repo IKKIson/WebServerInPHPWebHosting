4eee5ef1df543a364edb5fffce42b72c96082aff
IkkiSON <shout2517@naver.com>
2018-01-10 17:08:27 +0900

    1. Add this Education class Poster image 2. test.php add select query code

--- html/test.php
+++ html/test.php
@@ -1,11 +1,27 @@
 <?php
 					//호스트주소, id, pw
-$conn = mysql_connect('localhost', 'kpucom', 'e517e517');
-mysqli_connect($conn, 'kpucom');//데이터베이스 명
+$conn = mysqli_connect('localhost', 'shout2517', 'ihson83729405');
+mysqli_select_db($conn, 'shout2517');//데이터베이스 명
 
-$sql = "insert into notice (title, content) values ('제목', '내용')";
+//01 insert query
+//$sql = "insert into notice(title, content) values('titlesample2', 'descriptsample2')";
 
-$result = mysqli_query($conn, $sql);
-echo $result;
+//02 select query
+$sql = "select *from notice";
 
+if($conn){
+
+	$result = mysqli_query($conn, $sql);
+
+	//01 insert dml query
+	//var_dump($result);
+	
+	//02 select query
+	// select 조회
+	while($result = mysqli_fetch_arrary($result)) {
+		echo $row['title']."<br>";
+	}
+	
+	echo $result;
+}
 ?>
